3b125f14c15b6820f2baa95ca32bbf44c96bf21f
Daniel Calderon <danielcalderon@student.hartnell.edu>
2015-10-22 12:56:29 -0700

    FIxed the result.txt file.

--- Calderon.cpp
+++ Calderon.cpp
@@ -16,9 +16,16 @@ int main()
     int wordcount =0;
     int lettercount =0;
     int average = 0;
-    int word2 = 0;
+    int word1 = 0;
+	int word2 = 0;
 	int word3 = 0;
 	int word4 = 0;
+	int word5 = 0;
+	int word6 = 0;
+	int word7 = 0;
+	int word8 = 0;
+	int word9 = 0;
+	int word10 = 0;
     int wordM11 = 0;
     
     fin.open("gba.txt");
@@ -56,20 +63,46 @@ int main()
         
         newString+= word + " ";
          
-		if(word.length() == 2)
+		if(word.length() == 1)
+		{
+			word1++;
+		}
+		else if(word.length() == 2)
 		{
 			word2++;
-		
 		}
 		else if(word.length() == 3)
 		{
 			word3++;
-			
 		}
 		else if(word.length() == 4)
 		{
 			word4++;
-		}	
+		}
+		else if(word.length() == 5)
+		{
+			word5++;
+		}
+		else if(word.length() == 6)
+		{
+			word6++;
+		}
+		else if(word.length() == 7)
+		{
+			word7++;
+		}
+		else if(word.length() == 8)
+		{
+			word8++;
+		}
+		else if(word.length() == 9)
+		{
+			word9++;
+		}
+		else if(word.length() == 10)
+		{
+			word10++;
+		}
 		else if(word.length()>=11)
 		{
 			wordM11++;
@@ -113,9 +146,17 @@ int main()
     average = lettercount / wordcount;
     fout << "average characters per word:  " << average << endl;
     fout << "wordcount is " << wordcount-1 << endl;
-    fout << word2 << " words have the of length 2" << endl;
+    
+    fout << word1 << " words have the of length 1" << endl;
+	fout << word2 << " words have the of length 2" << endl;
 	fout << word3 << " words have the of length 3" << endl;
 	fout << word4 << " words have the of length 4" << endl;
+	fout << word5 << " words have the of length 5" << endl;
+	fout << word6 << " words have the of length 6" << endl;
+	fout << word7 << " words have the of length 7" << endl;
+	fout << word8 << " words have the of length 8" << endl;
+	fout << word9 << " words have the of length 9" << endl;
+	fout << word10 << " words have the of length 10" << endl;
     fout << wordM11 << " words have the of length 11 or longer" << endl;
     foutB << upperString << endl;
     foutC << upperFirst << endl;
